806b4d4322196f61e4cf9a7b723bd850ba8d3f3b
Julia RudenkoE <juliarudenko29@icloud.com>
2022-11-29 12:33:14 +0300

    add Example003 rundom

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA =37;
-int numberB =9;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10); 
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
